4b261da7a7fc14af15e20e203488ec0e87342f08
Kristofer Koishigawa <scissorsneedfoodtoo@gmail.com>
2019-09-25 17:35:02 +0900

    feat: Conditionally render comments on pages (#67)

---
+++ assets/built/comments.js
@@ -0,0 +1,2 @@
+function loadDiscourseComments(){alert("working")}window&&window.addEventListener&&window.addEventListener("load",function(){document.getElementById("trigger-load-comments").onclick=loadDiscourseComments});
+//# sourceMappingURL=comments.js.map
