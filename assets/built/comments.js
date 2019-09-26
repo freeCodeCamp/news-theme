fa5bfbbd905ed8374c491495f1b1a65da72594c3
Kristofer Koishigawa <scissorsneedfoodtoo@gmail.com>
2019-09-26 17:27:20 +0900

    Revert "feat: Conditionally render comments on pages" (#68)
    This reverts commit 4b261da7a7fc14af15e20e203488ec0e87342f08.

--- assets/built/comments.js
+++
@@ -1,2 +0,0 @@
-function loadDiscourseComments(){alert("working")}window&&window.addEventListener&&window.addEventListener("load",function(){document.getElementById("trigger-load-comments").onclick=loadDiscourseComments});
-//# sourceMappingURL=comments.js.map
