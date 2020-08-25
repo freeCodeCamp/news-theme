0f6cdae2f8006107309f6bedc62ef3e23af4436c
Kris Koishigawa <scissorsneedfoodtoo@gmail.com>
2020-08-25 19:18:35 +0900

    fix: update tooling and number of tags in post

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var gutil = require('gulp-util');
 var livereload = require('gulp-livereload');
 var postcss = require('gulp-postcss');
 var sourcemaps = require('gulp-sourcemaps');
-var zip = require('gulp-zip');
+var gulpZip = require('gulp-zip');
 var uglify = require('gulp-uglify');
 var filter = require('gulp-filter');
 
@@ -17,71 +17,68 @@ var customProperties = require('postcss-custom-properties');
 var easyimport = require('postcss-easy-import');
 
 var swallowError = function swallowError(error) {
-    gutil.log(error.toString());
-    gutil.beep();
-    this.emit('end');
+  gutil.log(error.toString());
+  gutil.beep();
+  this.emit('end');
 };
 
-var nodemonServerInit = function () {
-    livereload.listen(1234);
+const initLivereload = function (cb) {
+  livereload.listen(1234);
+  cb();
 };
 
-gulp.task('build', ['css', 'js'], function (/* cb */) {
-    return nodemonServerInit();
+const css = () => {
+  var processors = [
+    easyimport,
+    customProperties,
+    colorFunction(),
+    autoprefixer(),
+    cssnano()
+  ];
+
+  return gulp
+    .src('assets/css/*.css')
+    .on('error', swallowError)
+    .pipe(sourcemaps.init())
+    .pipe(postcss(processors))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('assets/built/'))
+    .pipe(livereload());
+};
+
+const js = () => {
+  var jsFilter = filter(['**/*.js'], { restore: true });
+
+  return gulp
+    .src('assets/js/*.js')
+    .on('error', swallowError)
+    .pipe(sourcemaps.init())
+    .pipe(jsFilter)
+    .pipe(uglify())
+    .pipe(jsFilter.restore)
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('assets/built/'))
+    .pipe(livereload());
+};
+
+const watch = (cb) => {
+  gulp.watch('assets/css/**', css);
+  cb();
+};
+
+const zip = gulp.series(gulp.parallel(css, js), function zipToDest() {
+  var targetDir = 'dist/';
+  var themeName = require('./package.json').name;
+  var filename = themeName + '.zip';
+
+  return gulp
+    .src(['**', '!node_modules', '!node_modules/**', '!dist', '!dist/**'])
+    .pipe(gulpZip(filename))
+    .pipe(gulp.dest(targetDir));
 });
 
-gulp.task('generate', ['css', 'js']);
+const build = gulp.series(gulp.parallel(css, js), initLivereload);
 
-gulp.task('css', function () {
-    var processors = [
-        easyimport,
-        customProperties,
-        colorFunction(),
-        autoprefixer({browsers: ['last 2 versions']}),
-        cssnano()
-    ];
+exports.zip = zip;
 
-    return gulp.src('assets/css/*.css')
-        .on('error', swallowError)
-        .pipe(sourcemaps.init())
-        .pipe(postcss(processors))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('assets/built/'))
-        .pipe(livereload());
-});
-
-gulp.task('js', function () {
-    var jsFilter = filter(['**/*.js'], {restore: true});
-
-    return gulp.src('assets/js/*.js')
-        .on('error', swallowError)
-        .pipe(sourcemaps.init())
-        .pipe(jsFilter)
-        .pipe(uglify())
-        .pipe(jsFilter.restore)
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('assets/built/'))
-        .pipe(livereload());
-});
-
-gulp.task('watch', function () {
-    gulp.watch('assets/css/**', ['css']);
-});
-
-gulp.task('zip', ['css', 'js'], function () {
-    var targetDir = 'dist/';
-    var themeName = require('./package.json').name;
-    var filename = themeName + '.zip';
-
-    return gulp.src([
-        '**',
-        '!node_modules', '!node_modules/**',
-        '!dist', '!dist/**'
-    ])
-        .pipe(zip(filename))
-        .pipe(gulp.dest(targetDir));
-});
-
-gulp.task('default', ['build'], function () {
-    gulp.start('watch');
-});
+exports.default = gulp.series(build, watch);
