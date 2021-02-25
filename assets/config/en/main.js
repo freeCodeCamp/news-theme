0022aaf8913bf0e52c6306d1040e2c821c048ae9
Kristofer Koishigawa <scissorsneedfoodtoo@gmail.com>
2021-02-25 21:03:26 +0900

    fix: separate config and footer links from translation files (#50)
    * fix: separate trending links and text from the locales JSON files
    
    * fix: combine trending info and footer links in one file
    
    * fix: move forum and donation links and algolia index name out of locale.json files
    
    * fix: simplify keyword meta
    
    * fix: learn to code cta button
    
    * fix: remove unused twitter entries from zh-cn.json
    
    * fix: simplify translation for twitter message
    
    * fix: remove period from object key
    
    * fix: rename donation-banner to banner, handle donation urls in one place
    
    * fix: rename other.js to main.js for main configs
    
    * fix: use async functions and dynamic imports to load config
    
    * fix: revert async/import changes, load all files with asset helper, change zh-cn.json to zh.json

---
+++ assets/config/en/main.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-unused-vars
+const enMain = {
+  'algolia-index-name': 'news',
+  'learn-to-code-cta-url': 'https://www.freecodecamp.org/learn/',
+  'forum-url': 'https://forum.freecodecamp.org/',
+  'donate-url': 'https://www.freecodecamp.org/donate/',
+  'banner-url': 'https://www.freecodecamp.org/'
+};
---
+++ assets/config/zh-cn/main.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-unused-vars
+const zhMain = {
+  'algolia-index-name': 'news-zh',
+  'learn-to-code-cta-url': 'https://chinese.freecodecamp.org/learn/',
+  'forum-url': 'https://chinese.freecodecamp.org/forum/',
+  'donate-url': 'https://chinese.freecodecamp.org/donate/',
+  'banner-url': 'https://chinese.freecodecamp.org/'
+};
