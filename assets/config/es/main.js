3c90a2231ad2547fab0e97939e20a24c921269f7
Kris Koishigawa <scissorsneedfoodtoo@gmail.com>
2021-02-28 17:55:22 +0900

    fix: Espanol banner link

--- assets/config/es/main.js
+++ assets/config/es/main.js
@@ -4,5 +4,5 @@ const esMain = {
   'learn-to-code-cta-url': 'https://www.freecodecamp.org/espanol/learn/',
   'forum-url': 'https://forum.freecodecamp.org/c/espanol/522',
   'donate-url': 'https://www.freecodecamp.org/espanol/donate/',
-  'banner-url': 'https://www.freecodecamp.org/espanol/'
+  'banner-url': 'https://chat.freecodecamp.org/home'
 };
