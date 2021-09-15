797a929f545a33cf911d8e215edad120d947250e
Kris Koishigawa <scissorsneedfoodtoo@gmail.com>
2021-09-15 12:12:58 +0900

    feat: enable links for pt-br

---
+++ assets/config/pt-br/main.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-unused-vars
+const ptBrMain = {
+  'algolia-index-name': 'news-pt-br',
+  'learn-to-code-cta-url': 'https://www.freecodecamp.org/portuguese/learn/',
+  'forum-url': 'https://forum.freecodecamp.org/c/portugues/534',
+  'donate-url': 'https://www.freecodecamp.org/portuguese/donate/',
+  'banner-url': 'https://chat.freecodecamp.org/home'
+};
