20d787684d1206135d83aa3c34e6ae36e21a5735
Ilenia <nethleen@gmail.com>
2021-09-29 04:59:29 +0200

    feat(i18n): italian news theme (#170)
    * created it files
    
    * edited files in assets/config
    
    * edited file in partials
    
    * translated files
    
    * Change link to it version
    
    Co-authored-by: Kristofer Koishigawa <scissorsneedfoodtoo@gmail.com>
    
    Co-authored-by: Kristofer Koishigawa <scissorsneedfoodtoo@gmail.com>

---
+++ assets/config/it/main.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-unused-vars
+const itMain = {
+  'algolia-index-name': 'news-it',
+  'learn-to-code-cta-url': 'https://www.freecodecamp.org/italian/learn/',
+  'forum-url': 'https://forum.freecodecamp.org/c/italiano/535',
+  'donate-url': 'https://www.freecodecamp.org/italian/donate/',
+  'banner-url': 'https://chat.freecodecamp.org/'
+};
