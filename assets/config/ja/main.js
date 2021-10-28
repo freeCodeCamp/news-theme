d99ce48464496f3cfaef7e20b1ab6b3ec3eaac8a
sidemt <siideemt@gmail.com>
2021-10-29 07:39:09 +0900

    Add Japanese translation

--- assets/config/ja/main.js
+++ assets/config/ja/main.js
@@ -2,7 +2,7 @@
 const jaMain = {
   'algolia-index-name': 'news',
   'learn-to-code-cta-url': 'https://www.freecodecamp.org/learn/',
-  'forum-url': 'https://forum.freecodecamp.org/',
+  'forum-url': 'https://forum.freecodecamp.org/c/japanese/552',
   'donate-url': 'https://www.freecodecamp.org/donate/',
   'banner-url': 'https://www.freecodecamp.org/'
 };
