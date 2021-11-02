9add5020fae031482410f185f7af271442de37dc
sidemt <siideemt@gmail.com>
2021-11-02 19:19:55 +0900

    feat(i18n): Add Japanese translation (#198)
    Co-authored-by: Kristofer Koishigawa <scissorsneedfoodtoo@gmail.com>

--- assets/config/ja/main.js
+++ assets/config/ja/main.js
@@ -1,8 +1,8 @@
 // eslint-disable-next-line no-unused-vars
 const jaMain = {
-  'algolia-index-name': 'news',
+  'algolia-index-name': 'news-ja',
   'learn-to-code-cta-url': 'https://www.freecodecamp.org/learn/',
-  'forum-url': 'https://forum.freecodecamp.org/',
+  'forum-url': 'https://forum.freecodecamp.org/c/japanese/552',
   'donate-url': 'https://www.freecodecamp.org/donate/',
   'banner-url': 'https://www.freecodecamp.org/'
 };
