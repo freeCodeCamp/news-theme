c7a14a550b369b822ae186a662677d6e0e7800d7
Ilenia <nethleen@gmail.com>
2022-01-18 16:49:11 +0100

    Change CTA in italian/news

--- assets/config/it/main.js
+++ assets/config/it/main.js
@@ -4,5 +4,5 @@ const itMain = {
   'learn-to-code-cta-url': 'https://www.freecodecamp.org/italian/learn/',
   'forum-url': 'https://forum.freecodecamp.org/c/italiano/535',
   'donate-url': 'https://www.freecodecamp.org/italian/donate/',
-  'banner-url': 'https://chat.freecodecamp.org/'
+  'banner-url': 'https://forum.freecodecamp.org/t/contribuire-a-tradurre-freecodecamp-in-italiano/479009/'
 };
